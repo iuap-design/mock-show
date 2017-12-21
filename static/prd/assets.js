dd88668d6e607a0875cadb191250f435ce3f4ad5
onvno <leewei2020@126.com>
2017-12-21 16:34:46 +0800

    add mock show

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@583f184d25fe982b6a5c.js","css":"index@583f184d25fe982b6a5c.css"},"lib":{"js":"lib@09301ab77ac3477ec0dc.js"},"lib2":{"js":"lib2@642deab2c885c3274a3e.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@b9f775b9cc02e7a453d2.js","css":"index@b9f775b9cc02e7a453d2.css"},"lib":{"js":"lib@748ef4fe82f919031748.js"},"lib2":{"js":"lib2@a4dace7863c4430ce18a.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
